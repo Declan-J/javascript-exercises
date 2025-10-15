3b1aa26aade8381363a3a5a02f8124c3b1cba67b
Dec <declanjacobs1@gmail.com>
2025-10-15 17:40:23 +0100

    Completed 10 & 11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,9 +1,27 @@
 const leapYears = function(year) {
 
     if(year % 4 == 0) {
-    
-    }}
-//Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years).
+        if(year % 100 == 0) {
+            if(year % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    else return false;
+}
+
+    console.log(leapYears(2000))
+
+//Create a function that determines whether or not a given year is a leap year. 
+
+// Leap years are determined by the following rules:
+
+// Leap years are years divisible by four (like 1984 and 2004). 
+// However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+// unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years).
+
 
 // Do not edit below this line
 module.exports = leapYears;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) * 5/9
+  return Number(temp.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = temp * 9/5 + 32
+  return Number(temp.toFixed(1))
 };
+
+console.log(convertToFahrenheit(121.22))
 
 // Do not edit below this line
 module.exports = {
